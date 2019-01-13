77c95737aa2513906cd5643f2617dc4ab2f503ca
Vincent Kok <vick.kok@live.com>
2019-01-13 22:20:55 +0800

    Updated 01_character_input.py v1.0
    Signed-off-by: Vincent Kok <vick.kok@live.com>

--- Practice-Python/01_character_input.py
+++ Practice-Python/01_character_input.py
@@ -1,4 +1,8 @@
-import datetime
+if __name__ == "__main__":  
+    name= raw_input("Please enter your name:")
+    age= int(input("Please enter your current age: "))
+    year=int(input("Current year:"))
 
-def character_input(age, name, year):
+    message = ('\n' + name + ', You will be 100 years old in year ' + str(year - age + 100))
 
+    print (message)

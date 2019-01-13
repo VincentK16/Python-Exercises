d7515c1456cdf86872568465d0a16c74d52015f2
Vincent Kok <vick.kok@live.com>
2019-01-13 22:28:18 +0800

    Delete 01_character_input.py
    The script been move inside the folder.

--- Practice-Python/01_character_input.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":  
-    name= raw_input("Please enter your name:")
-    age= int(input("Please enter your current age: "))
-    year=int(input("Current year:"))
-
-    message = ('\n' + name + ', You will be 100 years old in year ' + str(year - age + 100))
-
-    print (message)

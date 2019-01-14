332dcaf0241fcab5d96df48c6993d95e0e6ef9e2
Vincent Kok <vick.kok@live.com>
2019-01-14 21:41:51 +0800

    Added 02_Odd_or_Even.py
    Signed-off-by: Vincent Kok <vick.kok@live.com>

---
+++ Practice-Python/02_Odd_or_Even/02_Odd_or_Even.py
@@ -0,0 +1,13 @@
+#Odd or Even Number?
+
+num = input("Please enter a number:")
+
+#To check the type of num
+print(type(num))
+if num%2 == 1:
+   print ("You have entered an ODD number")
+
+elif num%2 == 0:
+   print ("You have entered an EVEN number")
+  
+

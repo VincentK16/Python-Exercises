a898ce30feac6309f3fbba8be26dd6734c4f56e0
Vincent Kok <vick.kok@live.com>
2019-01-15 23:51:24 +0800

    Added PyTricks: itertools.permutuation()
    Signed-off-by: Vincent Kok <vick.kok@live.com>

---
+++ PyTricks/01_itertools.permutations.py
@@ -0,0 +1,35 @@
+# itertools.permutations() generates permutations 
+# for an iterable. Time to brute-force those passwords ;-)
+
+import itertools
+
+for p in itertools.permutations('ABCD'):
+     print(p)
+
+'''
+Expected output:
+('A', 'B', 'C', 'D')
+('A', 'B', 'D', 'C')
+('A', 'C', 'B', 'D')
+('A', 'C', 'D', 'B')
+('A', 'D', 'B', 'C')
+('A', 'D', 'C', 'B')
+('B', 'A', 'C', 'D')
+('B', 'A', 'D', 'C')
+('B', 'C', 'A', 'D')
+('B', 'C', 'D', 'A')
+('B', 'D', 'A', 'C')
+('B', 'D', 'C', 'A')
+('C', 'A', 'B', 'D')
+('C', 'A', 'D', 'B')
+('C', 'B', 'A', 'D')
+('C', 'B', 'D', 'A')
+('C', 'D', 'A', 'B')
+('C', 'D', 'B', 'A')
+('D', 'A', 'B', 'C')
+('D', 'A', 'C', 'B')
+('D', 'B', 'A', 'C')
+('D', 'B', 'C', 'A')
+('D', 'C', 'A', 'B')
+('D', 'C', 'B', 'A')
+'''

71dc18b40c3b873b9c186d87242b7afa539ce0d5
Vincent Kok <vick.kok@live.com>
2019-01-16 22:53:45 +0800

    Updated print style using .format
    Signed-off-by: Vincent Kok <vick.kok@live.com>

--- Practice-Python/03_List_Less_Than_Ten/03_List_Less_Than_Ten.py
+++ Practice-Python/03_List_Less_Than_Ten/03_List_Less_Than_Ten.py
@@ -3,13 +3,16 @@ a = [1,1,2,3,5,8,13,21,34,55,89]
 
 newlist = filter(lambda x:x<5, a)
 
-print('Approach 1:')
-print(newlist)
+#print using .format style
+print('Approach 1: {}'.format(newlist))
+#print(newlist)
 
 #Approach 2 by using append method to a new list
 result = []
 for x in a:
     if x < 5:
        result.append(x)
+
+#normal printing style
 print("Approach 2:")
 print (result)

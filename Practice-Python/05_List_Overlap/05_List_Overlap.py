b898041011e055ea951988dffca1ecb97bac8cac
Vincent Kok <vick.kok@live.com>
2019-01-17 23:03:25 +0800

    Update 05_List_Overlap.py

--- Practice-Python/05_List_Overlap/05_List_Overlap.py
+++ Practice-Python/05_List_Overlap/05_List_Overlap.py
@@ -1,4 +1,6 @@
-
+#use 'set' to find out the unique elements in the list.
+#create an empty list for appending the overlapped number into it
+#loop over two list and double check for overlapped number 
 a = [1,1,2,3,5,8,13,21,34,55,89]
 b = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 

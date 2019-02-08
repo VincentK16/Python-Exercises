1df98051b10705afc0062b3b9b22d28307c875d2
Vincent Kok <vick.kok@live.com>
2019-02-08 15:24:47 +0800

    Updated 12_list_ends.py
    Signed-off-by: Vincent Kok <vick.kok@live.com>

--- Practice-Python/12_list_ends/12_list_ends.py
+++ Practice-Python/12_list_ends/12_list_ends.py
@@ -1,14 +1,17 @@
 import random
 
-def listend():
+def listend(list):
 
-   list_new=[list_1[0], list_1[-1]]
+   list_new=[list[0], list[-1]]
    return list_new
 
 if __name__ =='__main__':
 
    list_1=list(random.sample(range(0,20), random.randint(0,15)))
-   
-   result=listend()
-   print("List:{}".format(list_1))
+   result=listend(list_1)
+   list_2=list(random.sample(range(0,20), random.randint(0,15)))
+   result_2=listend(list_2)
+   print("List 1:{}".format(list_1))
+   print("List 2: {}".format(list_2))
    print("First and Last: {}".format(result))
+   print("First and Last for list 2: {}".format(result_2))
